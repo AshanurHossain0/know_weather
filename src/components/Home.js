d415d8f13f833a8ecf16782597fb1ff01c60ef46
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-06-05 15:24:15 +0530

    style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSun, faStreetView } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faCloudRain, faCloud, faStreetView } from '@fortawesome/free-solid-svg-icons'
 import { getData } from "../scripts/script.js";
 function Home() {
 
     const [data, setData] = useState({});
-    const [town, setTown] = useState("Kolkata");
+    const [town, setTown] = useState("Siliguri");
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+    setInterval(() => {
+        setTime(new Date().toLocaleTimeString())
+    }, 1000)
 
     useEffect(() => {
         getData(town)
             .then((response) => {
                 setData(response);
+                console.log(response);
             })
             .catch((err) => console.log(err));
-    },[town]);
+    }, [town]);
     return (
         <>
-            {(Object.keys(data).length !==0)?(
-                <div className="box">
+            {(Object.keys(data).length !== 0) ? (
 
-                <div id="weather-con">
-                    <FontAwesomeIcon id="icon" style={{ color: "#ff7700fe" }} icon={faSun} />
+                <div className="cont">
+                    <div className="search">
+                        <input type="text" placeholder="City Name" onChange={(e)=>{setTown(e.target.value)}} />
+                    </div>
+                    <div className="box">
+
+                        <div id="weather-con">
+                            {(data.data.weather[0].main === "Clear") ? (<FontAwesomeIcon id="icon" style={{ color: "#ff7700fe" }} icon={faSun} />)
+                                : (data.data.weather[0].main === "Clouds") ? (<FontAwesomeIcon id="icon" style={{ color: "#8b9393" }} icon={faCloud} />)
+                                    : (data.data.weather[0].main === "Rain") ? (<FontAwesomeIcon id="icon" style={{ color: "white" }} icon={faCloudRain} />)
+                                        : (<FontAwesomeIcon id="icon" style={{ color: "#ff7700fe" }} icon={faSun} />)
+                            }
+                            <p style={{ fontSize: "1rem" }}>{data.data.weather[0].main}</p>
+                        </div>
+                        <div className="info">
+                            <h2 className="location"> <FontAwesomeIcon icon={faStreetView} /> {data.data.name} , India </h2>
+                            <p id="date">{new Date().toLocaleDateString()} , {new Date().toLocaleTimeString()}</p>
+                            <h1 className="temp">{(data.data.main.temp - 273).toFixed(2)}°C</h1>
+                            <h3 className="temp-min-max">Min {(data.data.main.temp_min - 273).toFixed(2)}°C | Max {(data.data.main.temp_max - 273).toFixed(2)}°C</h3>
+                        </div>
+                        <div className="developer">
+                            <p>Developed by ~Ashanur</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="info">
-                    <h2 className="location"> <FontAwesomeIcon icon={faStreetView} /> {data.data.name} , India </h2>
-                    <p id="date">06 June 2023</p>
-                    <h1 className="temp">38°C</h1>
-                    <h3 className="temp-min-max">Min 30°C | Max 40°C</h3>
-                </div>
-                <div className="developer">
-                    <p>Developed by ~Ashanur</p>
-                </div>
-            </div>
-            ):(<div><h3>Loading...</h3></div>)}
+            ) : (<div><h3>Loading...</h3></div>)}
         </>
     )
 }
