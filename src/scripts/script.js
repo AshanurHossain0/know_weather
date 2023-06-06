f38b4116ca9e36b371e9b39cdaeab05799f16713
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-06-06 12:03:06 +0530

    env variable

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-const urlStr="https://api.openweathermap.org/data/2.5/weather?"
-const appId="f0ab79c947c667efe46f7cf0d4d9bb2e"
 export async function getData(town){
-    const res = await axios.get(`${urlStr}q=${town}&appid=${appId}`);
+    const res = await axios.get(`${process.env.REACT_APP_APPSTR}q=${town}&appid=${process.env.REACT_APP_APPID}`);
     return res;
 }
